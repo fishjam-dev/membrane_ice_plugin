8be1c16fd71d6861a45ecf677a818eb01cebc2e2
Jan Izydorczyk <jan.izydorczyk@swmansion.com>
2022-11-22 17:48:39 +0100

    Sync count supervisor children
    We cannot ensure counting children after setup of ice is done
    Current solution is ugly, but it works

--- test/membrane_ice_plugin/turn_cleaner_test.exs
+++ test/membrane_ice_plugin/turn_cleaner_test.exs
@@ -22,8 +22,8 @@ defmodule Membrane.ICE.TURNCleanerTest do
 
     assert_pipeline_play(pipeline)
 
-    assert %{specs: 1, active: 1, supervisors: 0, workers: 1} ==
-             DynamicSupervisor.count_children(turn_cleaner_sup)
+    assert %{specs: 1, active: 1, supervisors: 0, workers: 1} =
+             count_children_sync(turn_cleaner_sup)
 
     [{:undefined, turn_cleaner_pid, :worker, _modules}] =
       DynamicSupervisor.which_children(turn_cleaner_sup)
@@ -36,5 +36,34 @@ defmodule Membrane.ICE.TURNCleanerTest do
 
     assert %{specs: 0, active: 0, supervisors: 0, workers: 0} ==
              DynamicSupervisor.count_children(turn_cleaner_sup)
+  end
+
+  defp count_children_sync(sup) do
+    my_pid = self()
+
+    pid =
+      spawn(fn ->
+        send(my_pid, count_children_rec(sup))
+      end)
+
+    receive do
+      {:ok, value} ->
+        value
+    after
+      1000 ->
+        Process.exit(pid, :kill)
+        DynamicSupervisor.count_children(sup)
+    end
+  end
+
+  defp count_children_rec(sup) do
+    case DynamicSupervisor.count_children(sup) do
+      %{specs: 0, active: 0, supervisors: 0, workers: 0} ->
+        Process.sleep(10)
+        count_children_rec(sup)
+
+      non_zero_res ->
+        {:ok, non_zero_res}
+    end
   end
 end
